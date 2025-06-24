319b3d16ddc3823d8fe52e4aaee493b6d8e5a880
RamiroHR <19.rodriguez.89@gmail.com>
2025-06-24 18:48:41 +0200

    chore: install openzeppelin and hardhat ignition packages. Configure hardhat to include ignition. Update tsconfig file to include .js and hardhat config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,13 @@
-/* eslint-disable node/no-unpublished-import */
-import '@nomicfoundation/hardhat-toolbox';
-import 'dotenv/config';
+/* eslint-disable node/no-unpublished-require */
+/* eslint-disable @typescript-eslint/no-require-imports */
+require('@nomicfoundation/hardhat-toolbox');
+require('@nomicfoundation/hardhat-ignition-ethers');
+require('dotenv/config');
 
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+const config = {
   solidity: {
-    version: '0.8.19',
+    version: '0.8.20',
     settings: {
       // to reduce gas cost by optimizing the contract bytecode
       optimizer: {
@@ -16,30 +18,32 @@ module.exports = {
   },
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 31337
     },
     // local development
     localhost: {
       url: 'http://127.0.0.1:8545'
-    },
-    // tesnet deployment (real word testing scenario)
-    sepolia: {
-      url: process.env.SEPOLIA_URL || '',
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
-    },
-    // mainnet deployment (placeholder only - learning purpose)
-    mainnet:  {
-      url: process.env.MAINNET_URL || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
     }
+    // // tesnet deployment (real word testing scenario)
+    // sepolia: {
+    //   url: process.env.SEPOLIA_URL || '',
+    //   accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    // },
+    // // mainnet deployment (placeholder only - learning purpose)
+    // mainnet: {
+    //   url: process.env.MAINNET_URL || '',
+    //   accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    // }
   },
-  // To verify the contact in eteherscan automatically
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
-  },
+  // // To verify the contact in eteherscan automatically
+  // etherscan: {
+  //   apiKey: process.env.ETHERSCAN_API_KEY
+  // },
   // Display gas cost of each function during the tests
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: 'USD'
   }
 };
+
+module.exports = config;

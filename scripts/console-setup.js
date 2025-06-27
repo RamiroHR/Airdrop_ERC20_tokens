a10e9570f272cc054987243e240409953c2b1c6f
RamiroHR <19.rodriguez.89@gmail.com>
2025-06-27 13:28:49 +0200

    chore: move /fonts and /styles outside /app. Minor comment/uncomment changes in gitignore and console-setup.js

--- scripts/console-setup.js
+++ scripts/console-setup.js
@@ -4,8 +4,7 @@ const tokenName = 'DevToken';
 console.log(`Loading ${tokenName} console helpers...`);
 
 // Get contract instance
-// const tokenAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
-const tokenAddress = '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0';
+const tokenAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
 const DevToken = await ethers.getContractFactory(tokenName);
 const token = DevToken.attach(tokenAddress);

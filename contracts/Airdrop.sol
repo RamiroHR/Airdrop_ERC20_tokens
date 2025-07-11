8d5754cb92a2c0800abd91cd2dfe0d8d4aa18451
RamiroHR <19.rodriguez.89@gmail.com>
2025-07-11 19:45:38 +0200

    feat: UpdateAirdrop contract adding setMerkleRoot() and verifying merkle proof in Claim(). Backup old Airdrop contract to Airdrop_v0.sol file. Update Airdrop abi for app source code.

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -4,15 +4,16 @@ pragma solidity ^0.8.20;
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/utils/ReentrancyGuard.sol';
+import '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
 
 contract Airdrop is ReentrancyGuard, Ownable {
     // state variables decalarations
     IERC20 public immutable token; //of type Interface-ERC20 with methods: .transfer(), .balance(), .approuve(), etc
-    mapping(address => uint256) public airdropAmounts; //to store address:airdrop_amount
+    bytes32 public merkleRoot;
     mapping(address => uint256) public claimedAmounts; //to track address:claimed_amounts
 
     // Events
-    event AirdropSet(address user, uint256 amount);
+    event RootSet(bytes32 merkleRoot);
     event TokensClaimed(address user, uint256 amount);
 
     // Constructor executes once at deploy
@@ -21,28 +22,32 @@ contract Airdrop is ReentrancyGuard, Ownable {
         token = IERC20(_token); // cast address to interface (with methods)
     }
 
-    // set airdrop amount for a user - owner only
-    function setAirdropAmount(address user, uint256 amount) public onlyOwner {
-        require(user != address(0), "Invalid user address");
-        airdropAmounts[user] += amount;
-        emit AirdropSet(user, amount);
+    // set the merkle Root
+    function setMerkleRoot(bytes32 _merkleRoot) public onlyOwner {
+      merkleRoot = _merkleRoot;
+      emit RootSet(_merkleRoot);
     }
+
 
     // for user to claim theiir airdrop
-    function claimAll() public nonReentrant {
+    function claimAll(bytes32[] memory proof, uint256 amount) public nonReentrant {
         address user = msg.sender;
-        uint256 availableAmount = airdropAmounts[user] - claimedAmounts[user];
 
-        require(availableAmount > 0, "No tokens available to claim");
+        // verify if already claimed
+        require(claimedAmounts[user] == 0, "Already claimed");
+
+        // create leaf with double hash encoding for security (prevent lenght extension attacks)
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(user, amount))));
+
+        // verify proof
+        require(MerkleProof.verify(proof, merkleRoot, leaf), "Invalid proof");
 
         // Update claimed amount, then transfer (reversible if transfer fails)
-        claimedAmounts[user] += availableAmount;
-        require(token.transfer(user, availableAmount), "Token transfer failed");
+        claimedAmounts[user] = amount;
+        require(token.transfer(user, amount), "Token transfer failed");
 
-        emit TokensClaimed(user, availableAmount);
+        // emit event;
+        emit TokensClaimed(user, amount);
     }
 
-    function hasAirdrop(address user) public view returns (bool) {
-        return airdropAmounts[user] > 0;
-    }
 }
